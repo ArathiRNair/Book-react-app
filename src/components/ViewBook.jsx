df4353c2d33a3b341e28fb3db638671e8248a25d
ArathiRNair <arathiravinair@gmail.com>
2026-07-09 21:49:50 +0530

    view page is created

---
+++ src/components/ViewBook.jsx
@@ -0,0 +1,168 @@
+import React from 'react'
+
+const ViewBook = () => {
+  return (
+    <div>
+<div className="container">
+    <div className="row">
+        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <div className="row g-3">
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
+
+<div class="card" >
+  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8mszoHtk9cQ8BEMaqT9PTksaOPFoYkvsrWnxe6tkhcw&s=10" class="card-img-top" alt="..."></img>
+  <div class="card-body">
+    <h5 class="card-title">Percy Jackson</h5>
+      </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">Author:Rick Riordan</li>
+    <li class="list-group-item">Price:450</li>
+    <li class="list-group-item">   <a href="#" class="btn btn-success">Add To Cart</a></li>
+  </ul>
+</div>
+
+                </div>
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
+
+<div class="card" >
+  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8mszoHtk9cQ8BEMaqT9PTksaOPFoYkvsrWnxe6tkhcw&s=10" class="card-img-top" alt="..."></img>
+  <div class="card-body">
+    <h5 class="card-title">Percy Jackson</h5>
+      </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">Author:Rick Riordan</li>
+    <li class="list-group-item">Price:450</li>
+    <li class="list-group-item">   <a href="#" class="btn btn-success">Add To Cart</a></li>
+  </ul>
+</div>
+
+                </div>
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
+
+<div class="card" >
+  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8mszoHtk9cQ8BEMaqT9PTksaOPFoYkvsrWnxe6tkhcw&s=10" class="card-img-top" alt="..."></img>
+  <div class="card-body">
+    <h5 class="card-title">Percy Jackson</h5>
+      </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">Author:Rick Riordan</li>
+    <li class="list-group-item">Price:450</li>
+    <li class="list-group-item">   <a href="#" class="btn btn-success">Add To Cart</a></li>
+  </ul>
+</div>
+
+                </div>
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
+
+<div class="card" >
+  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8mszoHtk9cQ8BEMaqT9PTksaOPFoYkvsrWnxe6tkhcw&s=10" class="card-img-top" alt="..."></img>
+  <div class="card-body">
+    <h5 class="card-title">Percy Jackson</h5>
+      </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">Author:Rick Riordan</li>
+    <li class="list-group-item">Price:450</li>
+    <li class="list-group-item">   <a href="#" class="btn btn-success">Add To Cart</a></li>
+  </ul>
+</div>
+
+                </div>
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
+
+<div class="card" >
+  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8mszoHtk9cQ8BEMaqT9PTksaOPFoYkvsrWnxe6tkhcw&s=10" class="card-img-top" alt="..."></img>
+  <div class="card-body">
+    <h5 class="card-title">Percy Jackson</h5>
+      </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">Author:Rick Riordan</li>
+    <li class="list-group-item">Price:450</li>
+    <li class="list-group-item">   <a href="#" class="btn btn-success">Add To Cart</a></li>
+  </ul>
+</div>
+
+                </div>
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
+
+<div class="card" >
+  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8mszoHtk9cQ8BEMaqT9PTksaOPFoYkvsrWnxe6tkhcw&s=10" class="card-img-top" alt="..."></img>
+  <div class="card-body">
+    <h5 class="card-title">Percy Jackson</h5>
+      </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">Author:Rick Riordan</li>
+    <li class="list-group-item">Price:450</li>
+    <li class="list-group-item">   <a href="#" class="btn btn-success">Add To Cart</a></li>
+  </ul>
+</div>
+
+                </div>
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
+
+<div class="card" >
+  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8mszoHtk9cQ8BEMaqT9PTksaOPFoYkvsrWnxe6tkhcw&s=10" class="card-img-top" alt="..."></img>
+  <div class="card-body">
+    <h5 class="card-title">Percy Jackson</h5>
+      </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">Author:Rick Riordan</li>
+    <li class="list-group-item">Price:450</li>
+    <li class="list-group-item">   <a href="#" class="btn btn-success">Add To Cart</a></li>
+  </ul>
+</div>
+
+                </div>
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
+
+<div class="card" >
+  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8mszoHtk9cQ8BEMaqT9PTksaOPFoYkvsrWnxe6tkhcw&s=10" class="card-img-top" alt="..."></img>
+  <div class="card-body">
+    <h5 class="card-title">Percy Jackson</h5>
+      </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">Author:Rick Riordan</li>
+    <li class="list-group-item">Price:450</li>
+    <li class="list-group-item">   <a href="#" class="btn btn-success">Add To Cart</a></li>
+  </ul>
+</div>
+
+                </div>
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
+
+<div class="card" >
+  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8mszoHtk9cQ8BEMaqT9PTksaOPFoYkvsrWnxe6tkhcw&s=10" class="card-img-top" alt="..."></img>
+  <div class="card-body">
+    <h5 class="card-title">Percy Jackson</h5>
+      </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">Author:Rick Riordan</li>
+    <li class="list-group-item">Price:450</li>
+    <li class="list-group-item">   <a href="#" class="btn btn-success">Add To Cart</a></li>
+  </ul>
+</div>
+
+                </div>
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
+
+
+                    <div class="card" >
+  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8mszoHtk9cQ8BEMaqT9PTksaOPFoYkvsrWnxe6tkhcw&s=10" class="card-img-top" alt="..."></img>
+  <div class="card-body">
+    <h5 class="card-title">Percy Jackson</h5>
+      </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">Author:Rick Riordan</li>
+    <li class="list-group-item">Price:450</li>
+    <li class="list-group-item">   <a href="#" class="btn btn-success">Add To Cart</a></li>
+  </ul>
+</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+    </div>
+  )
+}
+
+export default ViewBook
